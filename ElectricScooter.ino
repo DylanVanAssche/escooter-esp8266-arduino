3dc42990679a2dad021903744953acc3182e9a6a
Dylan Van Assche <dylan.van.assche@protonmail.com>
2017-05-13 15:18:13 +0200

    Cleaned up a little bit (2)

--- ElectricScooter.ino
+++ ElectricScooter.ino
@@ -1,68 +1,84 @@
-/*
-  This a simple example of the aREST Library for the ESP8266 WiFi chip.
-  See the README file for more details.
-
-  Written in 2015 by Marco Schwartz under a GPL license.
-*/
-
-// Import required libraries
 #include <ESP8266WiFi.h>
 #include <EEPROM.h>
 #include <aREST.h>
 #include <SimpleDHT.h>
 #include <Servo.h>
 
+/*
+   ******************************************
+   *                                        *
+   *                                        *
+   *--------------> E-SCOOTER <-------------*
+   *                                        *
+   *              V 1.0 RELEASE             *
+   ******************************************
+   THIS IS A RELEASE! I'm never resposibly for any damage to your stuff! This version is tested on a NodeMCU V1.0 ESP8266.
+   Build your own DIY electric scooter and control it with your Sailfish OS smartphone!
+   The sketch is based on the Arduino examples which are delivered together with the ESP8266 Arduino core and the aREST library.
+   
+ >>> FEATURES:
+     ---------
+ 
+ * Control your DIY electric scooter from your Sailfish OS smartphone.
+ * Simple HTTP API
+ * Arduino compatible and uses the widly available & cheap ESP8266 WiFi module as CPU & WiFi controller.
+ * Uses WPA/WEP security when creating it's access point.
+ 
+ 
+ >>> IMPORTANT NOTES: 
+     ----------------   
+ * There are a lot of free pins on the ESP8266 so you can use them for other stuff if you like.
+ * Feel free to modify these sketches but don't hijack them ;-)
+ * Commercial use? Contact me! Released under GPLv3.
+   (c) Dylan Van Assche (2016) developer of the RGB-WIFI Arduino sketch & Sailfish OS app.
+*/
+
+// Initialize instances
 Servo ESC;
-
-// Create aREST instance
 aREST rest = aREST();
-
-// DHT11 sensor
 SimpleDHT11 dhtSensor;
 
 // WiFi parameters
-const char* ssid = "MyScooter";
-const char* password = "ThisJustRocks!";
-
-// The port to listen for incoming TCP connections
 #define LISTEN_PORT           80
+const char* ssid      =       "MyScooter";
+const char* password  =       "ThisJustRocks!";
 
-#define CODE  7151
-#define ACCEL_RATE 100
-#define DHT_RATE 5000
-#define SPEEDTHROTTLE_MIN 0
-#define SPEEDTHROTTLE_MAX 370
-#define NOISE_REDUCTION 2
-#define ESC_MIN 30
-#define ESC_MAX 160
-#define ESC_DECREASE_STEP 6
-#define ESC_INCREASE_STEP 3
-#define EEPROM_MAX_SPEED 1
-#define EEPROM_LIGHTS 2
+#define CODE                  7151
+#define ACCEL_RATE            100
+#define DHT_RATE              5000
+#define SPEEDTHROTTLE_MIN     0
+#define SPEEDTHROTTLE_MAX     370
+#define NOISE_REDUCTION       2
+#define ESC_MIN               30
+#define ESC_MAX               160
+#define ESC_DECREASE_STEP     6
+#define ESC_INCREASE_STEP     3
+#define EEPROM_MAX_SPEED      1
+#define EEPROM_LIGHTS         2
 
 // Pins
-#define LED_RED D6
-#define LED_GREEN D7
-#define LED_ESP8266 D4
-#define ESC_MOTOR D0
-#define DHT11 D3
-#define FRONTLIGHT D5
-#define BACKLIGHT D8
-#define THROTTLE A0
+#define LED_RED               D6
+#define LED_GREEN             D7
+#define LED_ESP8266           D4
+#define ESC_MOTOR             D0
+#define DHT11                 D3
+#define FRONTLIGHT            D5
+#define BACKLIGHT             D8
+#define THROTTLE              A0
 
 // Create an instance of the server
 WiFiServer server(LISTEN_PORT);
 
-long previousMillisDHT = 0;
-long previousMillisESC = 0;
+long previousMillisDHT =      0;
+long previousMillisESC =      0;
 
 // Variables to be exposed to the API
-int temperatureDHT = 0;
-int humidityDHT = 0;
-int enabled = 0;
-int maxSpeed = ESC_MAX;
-int ESCValue = ESC_MIN;
-int ESCValueOld = ESCValue;
+int temperatureDHT     =      0;
+int humidityDHT        =      0;
+int enabled            =      0;
+int maxSpeed           =      ESC_MAX;
+int ESCValue           =      ESC_MIN;
+int ESCValueOld        =      ESCValue;
 
 // Declare functions to be exposed to the API
 int lock(String code);
@@ -71,43 +87,42 @@ int speedLimit(String speed);
 
 void setup(void)
 {
-  // Start Serial
+  // Init Serial
   Serial.begin(115200);
-  EEPROM.begin(16); //init ESP8266 EEPROM 16 bytes
-  maxSpeed = EEPROM.read(EEPROM_MAX_SPEED) * 4;
+
+  // Init ESP8266 EEPROM 16 bytes
+  EEPROM.begin(16);
+  
+  // Set pins
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_ESP8266, OUTPUT);
   pinMode(FRONTLIGHT, OUTPUT);
   pinMode(BACKLIGHT, OUTPUT);
 
+  // Set default values
+  maxSpeed = EEPROM.read(EEPROM_MAX_SPEED)*4; //Save as byte so *4
   digitalWrite(LED_RED, HIGH); // Locked state
   digitalWrite(FRONTLIGHT, EEPROM.read(EEPROM_LIGHTS));
   digitalWrite(BACKLIGHT, EEPROM.read(EEPROM_LIGHTS));
 
-  // Init variables and expose them to REST API
+  // Expose variables and functions to REST API
   rest.variable("temperature", &temperatureDHT);
   rest.variable("humidity", &humidityDHT);
   rest.variable("enabled", &enabled);
   rest.variable("speed", &maxSpeed);
 
-  // Function to be exposed
   rest.function("lock", lock);
   rest.function("lights", lights);
   rest.function("speedlimit", speedLimit);
 
-  // Give name & ID to the device (ID should be 6 characters long)
+  // Device ID and name
   rest.set_id("1");
   rest.set_name("E-Scooter");
 
-  // Setup WiFi network
+  // Setup WiFi AP and start server
   WiFi.softAP(ssid, password);
-  Serial.println("");
-  Serial.println("WiFi created");
-
-  // Start the server
   server.begin();
-  Serial.println("Server started");
 
   // Print the IP address
   IPAddress myIP = WiFi.softAPIP();
@@ -116,8 +131,10 @@ void setup(void)
 }
 
 void loop() {
-  // Handle REST calls
+  // Update ESC values
   updateESC();
+
+  // Handle REST API
   WiFiClient client = server.available();
   if (!client) {
     return;
@@ -125,23 +142,29 @@ void loop() {
   while (!client.available()) {
     delay(1);
   }
-  readDHT();
+  
+  readDHT(); // Update DHT values before sending them to the client
   rest.handle(client);
 }
 
 int readDHT() {
-  if (millis() - previousMillisDHT > DHT_RATE) { // sampling rate
+  if (millis() - previousMillisDHT > DHT_RATE) { // Sampling rate is limited
     previousMillisDHT = millis();
+    
     byte temp;
     byte humidi;
-    if (dhtSensor.read(DHT11, &temp, &humidi, NULL)) {
+    
+    if (dhtSensor.read(DHT11, &temp, &humidi, NULL)) { // Check if read was OK
       Serial.println("[ERROR] DHT11 reading failed!");
       return 0;
     }
-    temperatureDHT = (int)temp;
+    temperatureDHT = (int)temp; // Update the real variables when OK
     humidityDHT = (int)humidi;
+    
     return 1;
   }
+  
+  return 0;
 }
 
 int updateESC() {
@@ -151,19 +174,19 @@ int updateESC() {
 
     digitalWrite(LED_ESP8266, LOW); // Turn LED on when we update the ESC (active low)
 
-    int ESCValue = analogRead(THROTTLE);
+    int ESCValue = analogRead(THROTTLE); // Get our ESCValue from the speedthrottle
     ESCValue = constrain(map(ESCValue, SPEEDTHROTTLE_MIN, SPEEDTHROTTLE_MAX, ESC_MIN, maxSpeed), ESC_MIN, maxSpeed); //Limit speed to maximum speed which can be set by calling /speedlimit?params=MAXSPEED
 
     if ((ESCValue > ESCValueOld + NOISE_REDUCTION) && enabled ) { // Accelerate (only when enabled)
       ESCValueOld = ESCValueOld + ESC_INCREASE_STEP;
-      Serial.println("+");
+      
       if (ESCValue < ESCValueOld) { // Overflow encounter
         ESCValueOld = ESCValue;
       }
     }
     else if (ESCValue < ESCValueOld - NOISE_REDUCTION) { //Deaccelerate
       ESCValueOld = ESCValueOld - ESC_DECREASE_STEP;
-      Serial.println("-");
+
       if (ESCValue > ESCValueOld) { // Underflow encounter
         ESCValueOld = ESCValue;
       }
@@ -181,7 +204,7 @@ int updateESC() {
   return 0;
 }
 
-// Custom function accessible by the API
+// (Un)lock the scooter
 int lock(String code) {
 
   if (code.toInt() == CODE) {
@@ -197,18 +220,19 @@ int lock(String code) {
   return 0;
 }
 
+// Turn the lights ON/OFF on the scooter
 int lights(String state) {
   if (!enabled) { //Only when scooter is enabled
     return 0;
   }
 
-  if (state.toInt() == 0) {
+  if (state.toInt() == 0) { // OFF
     digitalWrite(FRONTLIGHT, LOW);
     digitalWrite(BACKLIGHT, LOW);
     EEPROM.write(EEPROM_LIGHTS, 0);
     return 0;
   }
-  digitalWrite(FRONTLIGHT, HIGH);
+  digitalWrite(FRONTLIGHT, HIGH); // ON
   digitalWrite(BACKLIGHT, HIGH);
 
   // Save state
@@ -218,6 +242,7 @@ int lights(String state) {
   return 1;
 }
 
+// Set speedlimit
 int speedLimit(String speed) {
   if (!enabled) { //Only when scooter is enabled
     return 0;
@@ -232,6 +257,7 @@ int speedLimit(String speed) {
   return 1;
 }
 
+// Arm the ESC when scooter is enabled
 int armESC(int state) {
   if (state == 1) {
     ESC.attach(ESC_MOTOR);
